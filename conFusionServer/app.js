a3c3e3f86b0f9fca30866218fed02a409f17d606
Sarthak Srivastava <2016ceb1017@iitrpr.ac.in>
2020-07-23 13:29:54 +0530

    File Uplaod

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -14,8 +14,10 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
+
 
 const Dishes = require('./models/dishes');
 const { signedCookies } = require('cookie-parser');
@@ -58,6 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promo',promoRouter);
 app.use('/leader',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 
 // catch 404 and forward to error handler
