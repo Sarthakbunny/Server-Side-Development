ce504b7a93093e119102c24992bb11f45caffcfd
Sarthak Srivastava <2016ceb1017@iitrpr.ac.in>
2020-07-27 11:52:49 +0530

    OAuth using passport-facebook-token

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -4,8 +4,10 @@ var User  = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
-var config = require('./config');;
+var config = require('./config');const user = require('./models/user');
+;
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -58,3 +60,30 @@ exports.verifyadmin = function(req, res, next) {
       next(err);
     }
   };
+
+  exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        user.findOne({facebookId: profile.id }, (err, user) =>{
+            if (err){
+                return done(err, false);
+            }
+            if(!err && user!=null) {
+                return done(null, user);
+            }
+            else{
+                user = new User({ username: profile.displayName});
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) =>{
+                    if(err)
+                        return done(err, null);
+                    else    
+                        return done(null, user);
+                })
+            }
+        })
+    }
+  ))
--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '309114217116937',
+        clientSecret: '131115828836a9b19c1bd7b99798f829'
+    }
 }
